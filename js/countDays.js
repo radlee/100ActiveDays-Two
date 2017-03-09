18fa907ac3107d45ffe79d1860dec58e6b59cd3f
radlee <lolito@projectcodex.co>
2017-03-09 10:17:31 +0200

    Counts From Day #1 up to Day #100 and then display the message that the challenge is completed and resets the clock and time to zero

--- js/countDays.js
+++ js/countDays.js
@@ -1,29 +1,29 @@
 function calculateDays() {
     var workout = document.getElementById("work_out").value;
     var duration = document.getElementById("w_duration").value;
-
     var hundredthDay = new Date();
-
+    //Set the Hundredth Day--
     hundredthDay.setDate(hundredthDay.getDate() + 100);
-
     var interval = setInterval(function() {
       var now = new Date();
       var distance = hundredthDay - now;
-      console.log(distance);
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      document.getElementById("result").innerHTML = days + " Days " + hours + " Hours " +
-       minutes + " Minuutes " + seconds + " Seconds";
-console.log("---now----");
-console.log(now);
-console.log("----hundredthDay----");
-console.log(hundredthDay);
+      //Reset Current time to Zero and Start Countng Up to Day #100--
+      var today = (100-days);
+      var this_hour = (23-hours);
+      var this_minute = (59-minutes);
+      var this_second = (59-seconds);
+      document.getElementById("result").innerHTML = "Day #" + today;
+      document.getElementById("timer").innerHTML = "Hours " + this_hour + " Minutes " +
+       this_minute + " Sec " + this_second;
       if (now === hundredthDay) {
         clearInterval(interval);
-        document.getElementById("result").innerHTML = "Kick-Off";
+        document.getElementById("result").innerHTML = "100 Days Expired!";
+        document.getElementById("timer").innerHTML = "Hours " + this_hour + " Minutes " +
+         this_minute + " Sec " + this_second;
       }
     }, 1000);
 }
